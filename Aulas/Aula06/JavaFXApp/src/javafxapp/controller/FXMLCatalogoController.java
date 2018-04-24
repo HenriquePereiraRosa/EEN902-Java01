88ae756cb8dfc5603172dad5a0bf9e51abd4a9aa
Henrique <henrique.prosa@hotmail.com>
2018-04-23 22:22:15 -0300

    Added new items

--- Aulas/Aula06/JavaFXApp/src/javafxapp/controller/FXMLCatalogoController.java
+++ Aulas/Aula06/JavaFXApp/src/javafxapp/controller/FXMLCatalogoController.java
@@ -7,11 +7,14 @@ package javafxapp.controller;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafxapp.model.Contato;
 
 /**
  *
@@ -19,12 +22,15 @@ import javafx.scene.control.TableView;
  */
 public class FXMLCatalogoController implements Initializable {
     
+    
+    private ObservableList<Contato> contatos = FXCollections.observableArrayList();
+    
     @FXML
     private Label label, labelNome, labelSobrenome, labelCidade, 
             labelEstado, labelCep, labelDataNasc;
     
     @FXML
-    private TableView<?> tabaleContato;
+    private TableView<Contato> tabaleContato;
     
     @FXML
     private void handleButtonAction(ActionEvent event) {
@@ -34,7 +40,21 @@ public class FXMLCatalogoController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        populaTabela();
     }    
+    
+    public void populaTabela() {
+        contatos.add(new Contato("Henrique 1", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 2", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 3", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 4", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 5", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 6", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 7", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 8", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 9", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        contatos.add(new Contato("Henrique 10", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
+        tabaleContato.setItems(contatos);
+    }
     
 }
