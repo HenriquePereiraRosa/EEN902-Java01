1e686bd358a154bf75241e370cc83fba8f52807b
Henrique Rosa <henrique.prosa@hotmail.com>
2018-04-30 22:55:33 -0300

    tring to fix TableView error

--- Aulas/Aula06/JavaFXApp/src/javafxapp/controller/FXMLCatalogoController.java
+++ Aulas/Aula06/JavaFXApp/src/javafxapp/controller/FXMLCatalogoController.java
@@ -9,11 +9,12 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafxapp.model.Contato;
 
 /**
@@ -22,39 +23,39 @@ import javafxapp.model.Contato;
  */
 public class FXMLCatalogoController implements Initializable {
     
-    
     private ObservableList<Contato> contatos = FXCollections.observableArrayList();
-    
+        
     @FXML
-    private Label label, labelNome, labelSobrenome, labelCidade, 
+    private Label labelNome, labelSobrenome, labelCidade, 
             labelEstado, labelCep, labelDataNasc;
     
     @FXML
-    private TableView<Contato> tabaleContato;
+    private TableView<Contato> tableContato = new TableView<>();
     
     @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-        label.setText("Hello World!");
-    }
-    
+    private TableColumn<Contato, String> columnNome = new TableColumn<>("Nome");
+    private TableColumn<Contato, String> columnSobrenome = new TableColumn<>("Sobrenome");
+        
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        populaTabela();
+        columnNome.setCellValueFactory(new PropertyValueFactory<>("nome"));
+        columnSobrenome.setCellValueFactory(new PropertyValueFactory<>("sobrenome"));
+        tableContato.setItems(getItems());
+        tableContato.getColumns().addAll(columnNome,columnSobrenome);
     }    
     
-    public void populaTabela() {
+    public ObservableList<Contato> getItems() {
         contatos.add(new Contato("Henrique 1", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 2", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 3", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 4", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 5", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 6", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 7", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 8", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 9", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        contatos.add(new Contato("Henrique 10", "Rosa", "São Paulo", "SP", "08285140", "22/08/1990"));
-        tabaleContato.setItems(contatos);
+        contatos.add(new Contato("Henrique 2", "Rosa", "Rio Janeiro", "RJ", "08285140", "22/08/1991"));
+        contatos.add(new Contato("Henrique 3", "Rosa", "Belo Horizont", "MG", "08285140", "22/08/1992"));
+        contatos.add(new Contato("Henrique 4", "Rosa", "São Caetano do Sul", "SP", "08285140", "22/08/1993"));
+        contatos.add(new Contato("Henrique 5", "Rosa", "Diadema", "SP", "08285140", "22/08/1994"));
+        contatos.add(new Contato("Henrique 6", "Rosa", "Osasco", "SP", "08285140", "22/08/1994"));
+        contatos.add(new Contato("Henrique 7", "Rosa", "Guarulhos", "SP", "08285140", "22/08/1995"));
+        contatos.add(new Contato("Henrique 8", "Rosa", "São Paulo", "SP", "08285140", "22/08/1996"));
+        contatos.add(new Contato("Henrique 9", "Rosa", "São Paulo", "SP", "08285140", "22/08/1997"));
+        contatos.add(new Contato("Henrique 10", "Rosa", "São Paulo", "SP", "08285140", "22/08/1998"));
+        return contatos;
     }
     
 }
